21e6c040c812aba70a8f95ac85524b41ca912f90
Sayani Gupta <Sayani.Gupta@monash.edu>
2020-11-17 18:29:33 +1100

    incorporated interger input

--- R/distance_panel.R
+++ R/distance_panel.R
@@ -19,11 +19,11 @@ distance_panel <- function(sim_panel_quantiles,
 
   # range of i, j and k are defined in this way since some cyclic granularities start from 0 and others from 1 -  it was creating a problem while filtering in m1 and m2, where m2 was leading to a tibble of 0 rows and JS function was failing
 
-  if(class(sim_panel_quantiles$id_x) == "character")
+  if(class(sim_panel_quantiles$id_x) %in% c("character", "integer"))
   {
     sim_panel_quantiles$id_x = as.numeric(as.factor(sim_panel_quantiles$id_x)) %>% factor()
   }
-  if(class(sim_panel_quantiles$id_facet) == "character")
+  if(class(sim_panel_quantiles$id_facet) %in% c("character", "integer"))
   {
     sim_panel_quantiles$id_facet = as.numeric(as.factor(sim_panel_quantiles$id_facet)) %>% factor()
   }
