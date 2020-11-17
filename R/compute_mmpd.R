fad4a149f4e26883a75dc54d988ab07e2027cf54
Sayani Gupta <Sayani.Gupta@monash.edu>
2020-11-17 18:06:05 +1100

    savior - weird error
    https://stackoverflow.com/questions/62364853/error-in-parseoutfile-unexpected-end-of-input-bug-in-package-install-and

--- R/compute_mmpd.R
+++ R/compute_mmpd.R
@@ -27,7 +27,7 @@ compute_mmpd <- function(.data,
                         quantile_prob =
                         seq(0.01, 0.99, 0.01),
                         dist_ordered = TRUE)
-{{
+{
     if(!((gran_x %in%  names(.data) &
           (gran_facet %in%  names(.data)))))
     {
