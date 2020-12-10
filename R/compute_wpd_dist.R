b41f72412ff1a3eb3b90f20e3d55efade5099a45
Sayani Gupta <Sayani.Gupta@monash.edu>
2020-12-11 10:56:40 +1100

    wpd box cox transformed and normalised by log(number of distances)

---
+++ R/compute_wpd_dist.R
@@ -0,0 +1,59 @@
+#' Title computing all within and between facet distances between quantile categories given a data
+#'
+#' @param .data data for which mmpd needs to be calculated
+#' @param gran_x granularities mapped across x levels
+#' @param gran_facet granularities mapped across facetss
+#' @param response univarite response variable
+#' @param quantile_prob probabilities
+#' @param dist_ordered if categories are ordered
+#' @return
+#'
+#' @examples
+#' library(tidyverse)
+#' library(gravitas)
+#' library(parallel)
+#' sm <- smart_meter10 %>%
+#'   filter(customer_id %in% c("10017936"))
+#' gran_x <- "week_month"
+#' gran_facet <- "week_fortnight"
+#' v <- compute_wpd_dist(sm, gran_x, gran_facet,
+#'   response = general_supply_kwh
+#' )
+#' # month of the year not working in this setup
+#' @export compute_wpd_dist
+compute_wpd_dist <- function(.data,
+                                  gran_x = NULL,
+                                  gran_facet = NULL,
+                                  response = NULL,
+                                  quantile_prob =
+                                    seq(0.01, 0.99, 0.01),
+                                  dist_ordered = TRUE,
+                                  lambda = 0.67) {
+  if (!((gran_x %in% names(.data) &
+    (gran_facet %in% names(.data))))) {
+    .data <- .data %>%
+      create_gran(gran_x) %>%
+      create_gran(gran_facet)
+  }
+
+  all_dist_data <- suppressMessages(
+    .data %>%
+      as_tibble() %>%
+      select(!!gran_x, !!gran_facet, {{ response }}) %>%
+      rename("id_facet" = !!gran_facet) %>%
+      rename("id_x" = !!gran_x) %>%
+      rename("sim_data" = {{ response }}) %>%
+      mutate(sim_data = forecast::BoxCox(sim_data, lambda = "auto")) %>%
+      mutate(sim_data = scale(sim_data)) %>%
+      compute_quantiles(
+        quantile_prob =
+          quantile_prob
+      ) %>%
+      distance_all_pairwise(
+        dist_ordered = dist_ordered,
+        lambda = lambda
+      )
+  )
+
+  all_dist_data
+}
